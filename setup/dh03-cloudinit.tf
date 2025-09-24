b11ec26ae520f8d398431b3a543506bbc1d018e7
Cort Schaefer <cort@eschaefer.net>
2025-09-24 13:53:24 -0600

    add dh03 for clients

---
+++ setup/dh03-cloudinit.tf
@@ -0,0 +1,37 @@
+# cloud-init.tf - This is where I store cloud-init configuration
+
+# Source the Cloud Init Config file. NB: This file should be located 
+# in the "files" directory under the directory you have your Terraform
+# files in.
+data "template_file" "cloud_init_dh03" {
+  template = file("${path.module}/files/cloudinit-dh03.yml")
+
+  vars = {
+    ssh_key  = file("~/.ssh/id_ed25519.pub")
+    hostname = "dh03"
+    domain   = "local"
+    username  = var.proxmox_host_user
+    password_hash = var.proxmox_host_password_hash
+  }
+}
+
+# Create a local copy of the file, to transfer to Proxmox.
+resource "local_file" "cloud_init_dh03" {
+  content  = data.template_file.cloud_init_dh03.rendered
+  filename = "${path.module}/files/cloudinit-dh03.yml"
+}
+
+# Transfer the file to the Proxmox Host
+resource "null_resource" "cloud_init_dh03" {
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = var.proxmox_host_password
+    host = var.proxmox_host
+  }
+
+  provisioner "file" {
+    source      = local_file.cloud_init_dh03.filename
+    destination = "/var/lib/vz/snippets/cloud_init_dh03.yml"
+  }
+}
---
+++ setup/dh03.tf
@@ -0,0 +1,65 @@
+resource "proxmox_vm_qemu" "cloud_init_dh03" {
+  name        = "dh03" //var.vm_name
+  description = "Docker Host 03"
+  depends_on  = [null_resource.cloud_init_dh03]
+  # Node name has to be the same name as within the cluster
+  # this might not include the FQDN
+  target_node = var.proxmox_node
+
+  # The template name to clone this vm from
+  clone = var.template_name
+  # Activate QEMU agent for this VM
+  agent = 1
+
+  os_type = "cloud-init"
+  cpu {
+    cores   = 6
+    sockets = 1
+  }
+  #cpu = "host"
+  memory = 8192
+  scsihw = "virtio-scsi-single"
+
+  # Setup the disk
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = "warren"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size     = "250G"
+          storage  = "warren"
+          discard  = true
+          iothread = true
+        }
+      }
+    }
+  }
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = var.nic_name
+    #tag    = -1
+  }
+
+  # Setup the ip address using cloud-init.
+  boot = "order=scsi0"
+  # Keep in mind to use the CIDR notation for the ip.
+  ipconfig0 = "ip=dhcp,ip6=dhcp"
+  skip_ipv6 = true
+
+  lifecycle {
+    ignore_changes = [
+      ciuser,
+      sshkeys,
+      network
+    ]
+  }
+  cicustom = "user=local:snippets/cloud_init_dh03.yml"
+}
